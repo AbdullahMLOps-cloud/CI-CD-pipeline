c0e9742cd0e36e1110fbbf4db9651625f475ea18
Abdullah Khan <abiengineer@gmail.com>
2026-05-12 21:41:51 +0500

    added CI pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log('HEllo , its me Learning CI CD pipeline');
+console.log('This the the end of line')
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log('HEllo. ABdullah');
+setTimeout(() => { console.log('Waiting for three seconds'); }, 3000);
+console.log('This is the end of the code');
